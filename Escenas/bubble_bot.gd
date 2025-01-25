3dc939b2ce20c4d52db1e0e5c15aad3382306a4c
Jonathan D. Flores Franco <hiruma5436@gmail.com>
2025-01-25 01:56:47 -0600

    BubbleBot editado
    Se cambio el AnimationSprite2D a un AnimationPlayer, falta la lógica para cambio de animación

--- Escenas/bubble_bot.gd
+++ Escenas/bubble_bot.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 @export var speed = 300
-var sprite : AnimatedSprite2D
+@onready var sprite : Sprite2D
+@onready var animation_player = $AnimationPlayer
 
 func _ready() -> void:
-	sprite = $AnimatedSprite2D
+	sprite = $Sprite2D
+	animation_player.play("Idle")
 	
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -18,9 +20,14 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
-	
-func _input(event):
-	if event.is_action_pressed("spin"):
-		sprite.play("spin") 
-	else:
-		sprite.play("idle")
+
+# Falta agregar la logica para que haga el spin
+#func _input(event):
+	#if event.is_action_pressed("spin"):
+		#sprite.play("spin") 
+	#else:
+		#sprite.play("idle")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Idle":
+		animation_player.play("Idle")
