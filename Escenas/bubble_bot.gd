dbe1a86aabbeded0a935b5ecb1494135809279bb
Jonathan D. Flores Franco <hiruma5436@gmail.com>
2025-01-25 11:39:02 -0600

    Se agrego un AnimatedTree
    Al hacer las conexiones de las animaciones y mover al personaje la animación de idle se detiene pero el personaje se sigue moviendo

--- Escenas/bubble_bot.gd
+++ Escenas/bubble_bot.gd
@@ -3,15 +3,22 @@ extends CharacterBody2D
 @export var speed = 300
 @onready var sprite : Sprite2D
 @onready var animation_player = $AnimationPlayer
+@onready  var animation_tree: AnimationTree = $AnimationTree
+
+var input_direction: Vector2 = Vector2.ZERO
 
 const PROJECTILE = preload("res://Escenas/disparo_burbuja.tscn")
 
 func _ready() -> void:
 	sprite = $Sprite2D
-	animation_player.play("Idle")
+	#animation_player.play("Idle")
+	animation_tree.active = true
+	
+func _process(_delta: float) -> void:
+	update_animation_parameters()
 	
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	input_direction = Input.get_vector("left", "right", "up", "down")
 	if input_direction.x < 0:
 		sprite.flip_h = true
 	elif input_direction.x > 0:
@@ -40,5 +47,22 @@ func _input(event):
 		new_projectile.global_position = global_position
 		add_sibling(new_projectile)
 
+func update_animation_parameters():
+	if velocity == Vector2.ZERO:
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
+		
+	if Input.is_action_just_pressed("spin"):
+		animation_tree["parameters/conditions/spin"] = true
+	else:
+		animation_tree["parameters/conditions/spin"] = false
+		
+	animation_tree["parameters/Idle/blend_position"] = input_direction
+	animation_tree["parameters/Spin/blend_position"] = input_direction
+	animation_tree["parameters/Walk/blend_position"] = input_direction
+	
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	animation_player.play("Idle")
