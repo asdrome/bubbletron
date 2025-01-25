69d8e47b2bd25a43f7d342924216d7965f90911e
Jonathan D. Flores Franco <hiruma5436@gmail.com>
2025-01-25 15:39:01 -0600

    Se agregó enemigos
    Se agregaron las mosca
    
    El enemigo desaparece cuando es impactado por burbuja

---
+++ Escenas/Personajes/mosca.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+@export var speed = 200
+@export var movement_vector : Vector2 = Vector2.LEFT
+
+@onready var sprite : Sprite2D
+@onready var animation_player = $AnimationPlayer
+
+func _ready() -> void:
+	sprite = $Sprite2D
+	animation_player.play("Attack")
+
+func _process(delta: float) -> void:
+	translate(movement_vector * speed * delta)
+	position.y += sin(position.x * delta) * 4
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	animation_player.play("Attack")
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("burbuja"):
+		queue_free()
