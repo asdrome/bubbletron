1a991a3f2ed3285b995a56a71db57e5c3eba4fde
Jonathan D. Flores Franco <hiruma5436@gmail.com>
2025-01-24 21:56:18 -0600

    Movimiento de personaje
    Escena de nivel de dia y movimeinto del personaje

---
+++ Escenas/bubble_bot.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed = 400
+var sprite : AnimatedSprite2D
+
+func _ready() -> void:
+	sprite = $AnimatedSprite2D
+	
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	if input_direction.x < 0:
+		sprite.flip_h = true
+	elif input_direction.x > 0:
+		sprite.flip_h = false
+	
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
