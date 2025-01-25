93f6561eadfde55c7c2f79c2dadfee334c84ef05
Jonathan D. Flores Franco <hiruma5436@gmail.com>
2025-01-25 02:25:42 -0600

    Spin agregado
    Se agrego la lógica para que el personaje haga el ataque de spin

--- Escenas/bubble_bot.gd
+++ Escenas/bubble_bot.gd
@@ -21,13 +21,9 @@ func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 
-# Falta agregar la logica para que haga el spin
-#func _input(event):
-	#if event.is_action_pressed("spin"):
-		#sprite.play("spin") 
-	#else:
-		#sprite.play("idle")
+func _input(event):
+	if event.is_action_pressed("spin"):
+		animation_player.play("spin") 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "Idle":
-		animation_player.play("Idle")
+	animation_player.play("Idle")
